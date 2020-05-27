a1390e370d2c97c33d43151d47fe854e22b844c0
Pavel <pashamyl999@gmail.com>
2020-05-27 21:21:37 +0600

    Lesson 7 empty

---
+++ src/main/java/lesson7/annotation/MainL7.java
@@ -0,0 +1,17 @@
+package lesson7.annotation;
+
+public class MainL7 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void start(Class classStart){
+        //start @beforeSuite method (if starts twice - throw new RuntimeException
+
+        //start @Test methods
+
+        //start @AfterSuite method
+    }
+
+}
---
+++ src/main/java/lesson7/annotation/Test.java
@@ -0,0 +1,10 @@
+package lesson7.annotation;
+
+import java.lang.annotation.*;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test {
+    int priority() default 5;
+}
